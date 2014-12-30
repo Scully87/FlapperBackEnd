7088d322b194d3ca7f967f3ba16c59eb912968d8
Scully87 <scully87.ts@googlemail.com>
2014-12-30 18:11:57 +0000

    added var pg for app.js

--- app.js
+++ app.js
@@ -43,6 +43,19 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.FLAPPER_DEVELOPMENT, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.send(result.rows); }
+    });
+  });
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
